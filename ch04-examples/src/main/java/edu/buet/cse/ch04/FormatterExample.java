8761a3b9fc70de49f2314b4cbe767c866f2ca60a
Shamim Ahmed <shamim.buet.99@gmail.com>
2019-09-05 06:27:37 +1000

    Update the example on usage of DateTimeFormatter

--- ch04-examples/src/main/java/edu/buet/cse/ch04/FormatterExample.java
+++ ch04-examples/src/main/java/edu/buet/cse/ch04/FormatterExample.java
@@ -7,7 +7,9 @@ public class FormatterExample {
   public static void main(String... args) {
     LocalDateTime now = LocalDateTime.now();
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    
     System.out.println(formatter.format(now));
+
+    DateTimeFormatter formatter2 = DateTimeFormatter.ISO_DATE_TIME;
+    System.out.println(now.format(formatter2));
   }
 }
